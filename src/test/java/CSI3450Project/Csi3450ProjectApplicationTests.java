f686f09900bab10e91110ef0beb281950e751286
Richast <richard.pluta.12@gmail.com>
2020-11-14 02:24:46 +0000

    initial commit

---
+++ src/test/java/CSI3450Project/Csi3450ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package CSI3450Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csi3450ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
